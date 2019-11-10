a3aa7dcc6d2de27d6245103f1317ad6ada5150bd
FrancaTM <francatm@outlook.com>
2019-11-10 18:10:31 -0300

    add dropdown button

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -8,6 +8,8 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PrintScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +46,27 @@ class _PrintScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+                value: selectedCurrency,
+                items: [
+                  DropdownMenuItem(
+                    child: Text('USD'),
+                    value: 'USD',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('EUR'),
+                    value: 'EUR',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('GBP'),
+                    value: 'GBP',
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    selectedCurrency = value;
+                  });
+                }),
           ),
         ],
       ),
